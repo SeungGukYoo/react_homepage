37265424e01ea7181a081527edea30a99cb6ac61
Yoo Seung Guk <dbtmd878@naver.com>
2022-12-29 21:52:15 +0900

    feat: 메인 bannel 컴포넌트 추가

---
+++ src/components/main/Bannel_main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function BannelMain() {
+  return (
+    <section className='bannel_main'>
+      <div className='inner'>
+        <video autoPlay muted loop>
+          <source src={`${process.env.PUBLIC_URL}/videos/main_video.mp4`} />
+        </video>
+        <h1>An untraditional hotel</h1>
+      </div>
+    </section>
+  );
+}
+
+export default BannelMain;
