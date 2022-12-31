de81ce9b100cfb2731dcabc093e38e6e42848118
Yoo Seung Guk <dbtmd878@naver.com>
2022-12-31 17:29:35 +0900

    feat: 모든 메인 페이지 컴포넌트를 감싸는 루트컴포넌트 추가

---
+++ src/components/main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../common/Header';
+import AboutMain from './About_main';
+import BannelMain from './Bannel_main';
+import Figure from './Figure';
+import GalleryMain from './Gallery_main';
+import InfoMain from './Info_main';
+
+function Main() {
+  return (
+    <main>
+      <Header type='main' />
+      <Figure />
+      <AboutMain />
+      <BannelMain />
+      <InfoMain />
+      <GalleryMain />
+    </main>
+  );
+}
+
+export default Main;
