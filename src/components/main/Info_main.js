8ad7b1369cfd6dcae7dc6df6b810946d8b423b77
Yoo Seung Guk <dbtmd878@naver.com>
2022-12-31 17:28:07 +0900

    style: br태그 span태그로 변경

--- src/components/main/Info_main.js
+++ src/components/main/Info_main.js
@@ -12,17 +12,18 @@ function InfoMain() {
               <span>Vipp Loft</span>
             </div>
             <h1>
-              Take a look <br /> at out rooms
+              <span>Take a look&nbsp;</span>
+              <span>at out rooms</span>
             </h1>
             <p>
-              The hotel is small with just three locations, but <br />
-              more are in the making. Treat yourself to a <br />
-              getaway in the Vipp Shelter surrounded by <br />
-              Swedish nature, go for maximized urbanity at the <br />
-              Vipp Loft in Copenhagen, or check into a hisoric <br />
-              Copenhagen landmark at Vipp Chimney House.
+              <span>The hotel is small with just three locations, but </span>
+              <span>more are in the making. Treat yourself to a </span>
+              <span>getaway in the Vipp Shelter surrounded by </span>
+              <span>Swedish nature, go for maximized urbanity at the </span>
+              <span>Vipp Loft in Copenhagen, or check into a hisoric </span>
+              <span>Copenhagen landmark at Vipp Chimney House.</span>
             </p>
-            <button>Read all facts</button>
+            <button className='globalButton'>Read all facts</button>
           </div>
         </article>
         <article className='info_image'>
