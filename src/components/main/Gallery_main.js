ac423168f705acab7619c6741da8eb2e125e5ae2
Yoo Seung Guk <dbtmd878@naver.com>
2022-12-31 17:28:44 +0900

    feat: 메인 갤러리 컴포넌트 추가

---
+++ src/components/main/Gallery_main.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function GalleryMain(props) {
+  return (
+    <section className='gallery_main'>
+      <div className='inner'>
+        <h1>
+          <span>Our latest </span>
+          <span>Instagrams</span>
+        </h1>
+        <p>Go to Instagram</p>
+        <div className='wrap'>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image1.jpg`} alt='' />
+          </article>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image2.jpg`} alt='' />
+          </article>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image3.jpg`} alt='' />
+          </article>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image4.jpg`} alt='' />
+          </article>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image5.jpg`} alt='' />
+          </article>
+          <article>
+            <img src={`${process.env.PUBLIC_URL}/images/gallery_image6.jpg`} alt='' />
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default GalleryMain;
