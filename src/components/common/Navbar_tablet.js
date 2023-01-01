d9fc9b91b3563f86f9d43f4a936eb2a84b75ba02
Yoo Seung Guk <dbtmd878@naver.com>
2023-01-02 00:13:19 +0900

    feat: 토글 메뉴 활성화 함수 추가

--- src/components/common/Navbar_tablet.js
+++ src/components/common/Navbar_tablet.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function NavbarTablet() {
+  const [open, setOpen] = useState(false);
+  const navRef = useRef(null);
+  const menuOpen = () => {
+    open ? (navRef.current = '') : (navRef.current = 'on');
+    setOpen((prev) => !prev);
+  };
+
   return (
     <>
-      <nav className='mobile_navbar'>
+      <nav className={`mobile_navbar ${navRef.current}`}>
         <h1>vipp</h1>
-        <ul>
+        <ul ref={navRef}>
           <li>
             <NavLink to='/department'>Our Hotels</NavLink>
           </li>
@@ -23,7 +30,7 @@ function NavbarTablet() {
           </li>
         </ul>
       </nav>
-      <FontAwesomeIcon icon={faBars} className='menu_btn' />
+      <FontAwesomeIcon icon={faBars} className='menu_btn' onClick={menuOpen} />
     </>
   );
 }
