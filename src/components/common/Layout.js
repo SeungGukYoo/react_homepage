535368c4c19a2e9a9012c51d55f7ab1964609d42
Yoo Seung Guk <dbtmd878@naver.com>
2023-01-02 00:11:01 +0900

    feat: 서브페이지 중복 레이아웃을 담당하는 컴포넌트 추가

---
+++ src/components/common/Layout.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from 'react';
+
+function Layout({ name, children }) {
+  const innerRef = useRef(null);
+  useEffect(() => {
+    innerRef.current.classList.add('on');
+  }, []);
+  return (
+    <section className={`content ${name}`}>
+      <figure></figure>
+      <div className='inner' ref={innerRef}>
+        <h1>{name}</h1>
+        {children}
+      </div>
+    </section>
+  );
+}
+
+export default Layout;
