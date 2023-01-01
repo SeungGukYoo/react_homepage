f826eba9644a19ff23362582d6a9089d34b1e00c
Yoo Seung Guk <dbtmd878@naver.com>
2023-01-02 00:20:38 +0900

    feat: offsetTop 값을 구하는 함수 구현

---
+++ src/components/common/Scroll.js
@@ -0,0 +1,8 @@
+export function Scroll(ref) {
+  const sectionArr = ref.current.querySelectorAll('.scrollContent');
+
+  const result = [...sectionArr].map((section) => section.offsetTop);
+  return result;
+}
+
+export function getSizeArray() {}
