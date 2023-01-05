c820ef5caaa0d7bd1fdd8b633e723af712d4faa1
Yoo Seung Guk <dbtmd878@naver.com>
2023-01-05 17:49:57 +0900

    feat: department 컴포넌트 추가

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,10 +1,93 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+
+import { useState } from 'react';
 import Layout from '../common/Layout';
 
 function Department(props) {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.PUBLIC_URL}/DB/members.json`)
+      .then((result) => setMembers(result.data.members));
+    console.log(members);
+  }, []);
   return (
-    <Layout name={'department'}>
-      <h1>this is department</h1>
+    <Layout name={'department'} className='department'>
+      <ul>
+        {members.map((member, idx) => {
+          return (
+            <li>
+              <div>
+                <div className='imgBox'>
+                  <img src={`${process.env.PUBLIC_URL}/images/department/${member.pic}`} alt='' />
+                </div>
+                <div className='img_description'>
+                  <h1>{member.name}</h1>
+                  <p>{member.position}</p>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+        {/* <li>
+          <div>
+            <div className='imgBox'>
+              <img src={`${process.env.PUBLIC_URL}/images/department/member1.jpg`} alt='' />
+            </div>
+            <div className='img_description'>
+              <h1>Alex</h1>
+              <p>Junior Engineer</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div className='imgBox'>
+              <img src={`${process.env.PUBLIC_URL}/images/department/member2.jpg`} alt='' />
+            </div>
+            <div className='img_description'>
+              <h1>Alex</h1>
+              <p>Junior Engineer</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div className='imgBox'>
+              <img src={`${process.env.PUBLIC_URL}/images/department/member3.jpg`} alt='' />
+            </div>
+            <div className='img_description'>
+              <h1>Alex</h1>
+              <p>Junior Engineer</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div className='imgBox'>
+              <img src={`${process.env.PUBLIC_URL}/images/department/member4.jpg`} alt='' />
+            </div>
+            <div className='img_description'>
+              <h1>Alex</h1>
+              <p>Junior Engineer</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div className='imgBox'>
+              <img src={`${process.env.PUBLIC_URL}/images/department/member5.jpg`} alt='' />
+            </div>
+            <div className='img_description'>
+              <h1>Alex</h1>
+              <p>Junior Engineer</p>
+            </div>
+          </div>
+        </li> */}
+      </ul>
     </Layout>
   );
 }
