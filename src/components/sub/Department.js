e44bfd1691be7ae0cbac76be3be490410446700b
Yoo Seung Guk <dbtmd878@naver.com>
2023-01-06 00:31:36 +0900

    style: 콘텐츠 추가

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -12,14 +12,81 @@ function Department(props) {
     axios
       .get(`${process.env.PUBLIC_URL}/DB/members.json`)
       .then((result) => setMembers(result.data.members));
-    console.log(members);
   }, []);
   return (
-    <Layout name={'department'} className='department'>
+    <Layout name={'Department'}>
+      <div className='company_description'>
+        <article>
+          <h2>
+            Much like human well-being <br />
+            the essence of vipp <br />
+            Architects work is found in <br />
+            <strong>
+              <u>balance-between</u>
+            </strong>
+            &nbsp;richness <br />
+            and restraint, between order <br /> and complexity
+          </h2>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sapiente voluptates
+            nam sint illum enim quod adipisci, nihil numquam maxime vitae dolore aut cupiditate
+            animi nemo eligendi quia, sunt ipsa corporis? Esse et debitis, accusamus tempora fugit
+            dolores blanditiis repellendus quidem reiciendis suscipit at mollitia? Veritatis
+            deserunt, incidunt, repudiandae commodi quisquam tempora sed in aut fugit doloremque
+            quibusdam, itaque iste at! Saepe dolor odio voluptatum debitis consectetur, aliquam nisi
+            et assumenda amet esse? Obcaecati mollitia fugit vel voluptatibus dicta dolorem
+            exercitationem nisi nulla maxime ipsam omnis, minus ea. Repudiandae inventore vero
+            cupiditate rem, laborum accusamus accusantium harum dolorem aut ipsam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat harum hic quidem totam
+            vitae esse dolores perferendis corrupti fugit similique? Expedita accusantium sint ipsam
+            nisi repellendus autem nobis. Consectetur optio porro non hic totam provident placeat
+            voluptate aliquid rerum beatae ab animi, aperiam sit, illum eum magnam culpa quibusdam.
+            Aspernatur, repudiandae adipisci deleniti dicta facere quod velit ut voluptatem est
+            fugiat possimus sapiente similique enim et aliquam veritatis iure reprehenderit.
+          </p>
+          <div>
+            <ul>
+              <li>
+                <h3>
+                  12 Years <br />
+                  Experience
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, unde?</p>
+              </li>
+              <li>
+                <h3>
+                  145 <br />
+                  Projects
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, unde?</p>
+              </li>
+              <li>
+                <h3>
+                  12 Awards <br />
+                  Winning
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, unde?</p>
+              </li>
+              <li>
+                <h3>
+                  39 Great <br />
+                  Clients
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, unde?</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </div>
+      <h3>VIPP PEOPLE</h3>
       <ul>
         {members.map((member, idx) => {
           return (
-            <li>
+            <li style={{ order: idx }}>
               <div>
                 <div className='imgBox'>
                   <img src={`${process.env.PUBLIC_URL}/images/department/${member.pic}`} alt='' />
@@ -32,61 +99,24 @@ function Department(props) {
             </li>
           );
         })}
-        {/* <li>
-          <div>
-            <div className='imgBox'>
-              <img src={`${process.env.PUBLIC_URL}/images/department/member1.jpg`} alt='' />
-            </div>
-            <div className='img_description'>
-              <h1>Alex</h1>
-              <p>Junior Engineer</p>
-            </div>
-          </div>
+        <li className='department_description'>
+          <h3>OUT CREATIVES AND BEST MEMBER</h3>
+          <p>
+            <span>
+              They drive the rudder of creativity with precision, following their sixth sense that
+            </span>
+            <span>
+              allows them to see the world in RAW and see what the others can't. To hear the sun
+            </span>
+            <span>
+              sizzling behind the sea horizon, to swi, in the endless immensity of the clouds and
+            </span>
+            <span>
+              smell the dandelions flowering in distant lands. They give birth to magical digital
+              experiences.
+            </span>
+          </p>
         </li>
-        <li>
-          <div>
-            <div className='imgBox'>
-              <img src={`${process.env.PUBLIC_URL}/images/department/member2.jpg`} alt='' />
-            </div>
-            <div className='img_description'>
-              <h1>Alex</h1>
-              <p>Junior Engineer</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div>
-            <div className='imgBox'>
-              <img src={`${process.env.PUBLIC_URL}/images/department/member3.jpg`} alt='' />
-            </div>
-            <div className='img_description'>
-              <h1>Alex</h1>
-              <p>Junior Engineer</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div>
-            <div className='imgBox'>
-              <img src={`${process.env.PUBLIC_URL}/images/department/member4.jpg`} alt='' />
-            </div>
-            <div className='img_description'>
-              <h1>Alex</h1>
-              <p>Junior Engineer</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div>
-            <div className='imgBox'>
-              <img src={`${process.env.PUBLIC_URL}/images/department/member5.jpg`} alt='' />
-            </div>
-            <div className='img_description'>
-              <h1>Alex</h1>
-              <p>Junior Engineer</p>
-            </div>
-          </div>
-        </li> */}
       </ul>
     </Layout>
   );
