726301adc03e372b534111d10e254d2a11c13e58
SeungGukYoo <dbtmd878@gmail.com>
2023-01-08 01:15:24 +0900

    feat: 버튼에 따른 서로 다른 비디오 호출 기능 및 SCSS 추가

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,3 +1,5 @@
+import { faChevronDown, faL } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React from 'react';
 import { useRef } from 'react';
@@ -9,51 +11,82 @@ function About(props) {
   const [videos, setVideos] = useState([]);
   const [button, setButton] = useState('1');
   const btnRefList = useRef(null);
+  const [show, setShow] = useState(false);
+  const btnInfo = useRef([
+    {
+      playId: 'PL8Yp2hhTedFZOl_wbDTw7Pmx9iAxk6A48',
+      maxResult: 18
+    },
+    {
+      playId: 'PL8Yp2hhTedFaB2O-x07LTTeScBKQkkKSb',
+      maxResult: 24
+    },
+    {
+      playId: 'PL8Yp2hhTedFYamvuIbGJjdF_YI_nZ2vf-',
+      maxResult: 15
+    }
+  ]);
+  const count = useRef(6);
 
-  const playId = 'PL8Yp2hhTedFZOl_wbDTw7Pmx9iAxk6A48';
-  const url =
-    'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL8Yp2hhTedFZOl_wbDTw7Pmx9iAxk6A48&maxResults=5&key=AIzaSyCzFsvPUv4oQBwHeCsYH1FxMueWv-GAiY8';
-
-  const getPlayList = async () => {
+  const getPlayList = async (btnInfo) => {
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${btnInfo.playId}&maxResults=${count.current}&key=AIzaSyCzFsvPUv4oQBwHeCsYH1FxMueWv-GAiY8`;
     const json = await axios.get(url);
     setVideos(json.data.items);
+    setTimeout(() => {
+      setShow(true);
+    }, 500);
   };
 
   const clickBtn = (e) => {
     if (e.target === e.currentTarget) return;
     if (button !== e.target.dataset.index) {
+      console.log('clickBtn');
+      count.current = 6;
       setButton(e.target.dataset.index);
+      setShow(false);
+      getPlayList(btnInfo.current[parseInt(e.target.dataset.index - 1)]);
     }
   };
 
+  const moreLoad = () => {
+    if (count === btnInfo.current[parseInt(button) - 1].maxResult) return;
+    console.log('moreload');
+    count.current += 6;
+    getPlayList(btnInfo.current[parseInt(button) - 1]);
+  };
+
   useEffect(() => {
-    console.log('ssss');
-    getPlayList();
-  }, []);
+    console.log('useEffct');
+    setShow(false);
+    getPlayList(btnInfo.current[parseInt(button) - 1]);
+  }, [btnInfo]);
   return (
     <Layout name={'Youtube'}>
       <ul className='button' ref={btnRefList} onClick={clickBtn}>
         <li data-index='1' className={button === '1' ? 'on' : 'off'}>
-          PART 1
+          Hotel
         </li>
         <li data-index='2' className={button === '2' ? 'on' : 'off'}>
-          PART 2
+          Attraction
         </li>
         <li data-index='3' className={button === '3' ? 'on' : 'off'}>
-          PART 3
+          Food
         </li>
       </ul>
 
-      <ul className='youtube_container'>
-        {videos.map((video, idx) => {
+      <ul className={`youtube_container ${show === true ? 'on' : 'off'}`}>
+        {videos.map((video) => {
           const title =
             video.snippet.title.length > 25
               ? video.snippet.title.substring(0, 25) + '...'
               : video.snippet.title;
-          const des =
+          let des =
             video.snippet.description.length > 200
               ? video.snippet.description.substring(0, 200) + '...'
               : video.snippet.description;
+          des.length === 0 &&
+            (des =
+              'Welcome to Details in Luxury and in this video we are going to give you guys an in depth look inside the vipp Hotel.');
           return (
             <li key={video.snippet.resourceId.videoId}>
               <p className='video_title'>{title}</p>
@@ -67,6 +100,11 @@ function About(props) {
           );
         })}
       </ul>
+      {count.current < btnInfo.current[parseInt(button) - 1].maxResult - 1 && (
+        <div className='btnBox'>
+          <FontAwesomeIcon onClick={moreLoad} icon={faChevronDown} className='moreBtn' />
+        </div>
+      )}
     </Layout>
   );
 }
