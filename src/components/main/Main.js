04ecc6527a8f6bd9837198180b6f563ca38224f5
SeungGukYoo <dbtmd878@gmail.com>
2023-01-08 23:13:58 +0900

    fix: 불필요한 코드 제거

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef } from 'react';
-import { Scroll } from '../common/Scroll';
+import React from 'react';
 import Header from '../common/Header';
 import AboutMain from './About_main';
 import BannelMain from './Bannel_main';
@@ -7,23 +6,10 @@ import Figure from './Figure';
 import GalleryMain from './Gallery_main';
 import InfoMain from './Info_main';
 
-function Main() {
-  const itemHeigthArr = useRef([]);
-  const mainRef = useRef(null);
-
-  // const print = () => {
-  //   console.log(mainRef.current);
-  // };
-  // useEffect(() => {
-  //   window.addEventListener('resize', print);
-  //   return () => {
-  //     window.removeEventListener('resize', print);
-  //   };
-  // }, []);
-
+function Main({ openState }) {
   return (
-    <main ref={mainRef}>
-      <Header type='main' />
+    <main>
+      <Header type='main' openState={openState} />
       <Figure />
       <AboutMain />
       <BannelMain />
