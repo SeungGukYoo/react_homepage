2e5289ec6a803ff5b98dc036c25dafc225c4afbd
SeungGukYoo <dbtmd878@gmail.com>
2023-01-09 16:16:42 +0900

    feat: 리덕스 연결

--- src/App.js
+++ src/App.js
@@ -11,9 +11,18 @@ import Contact from './components/sub/Contact';
 import './scss/style.scss';
 import NavbarTablet from './components/common/Navbar_tablet';
 import { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import * as types from './redux/actionType';
 
 function App() {
   const openState = useRef(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: types.YOUTUBE.start, Info: { type: 'interest', count: 6 } });
+    dispatch({ type: types.FLICKR.start });
+  }, [dispatch]);
 
   return (
     <>
