85563d19db896bb37d46d1b1888fd0a3ede932d5
SeungGukYoo <dbtmd878@gmail.com>
2023-01-09 16:17:26 +0900

    feat: flickr 데이터로부터 전송받은 이미지로 변경

--- src/components/main/Gallery_main.js
+++ src/components/main/Gallery_main.js
@@ -4,47 +4,53 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function GalleryMain(props) {
-  const images = [
-    {
-      source: '/images/gallery_image1.jpg'
-    },
-    {
-      source: '/images/gallery_image2.jpg'
-    },
-    {
-      source: '/images/gallery_image3.jpg'
-    },
-    {
-      source: '/images/gallery_image4.jpg'
-    },
-    {
-      source: '/images/gallery_image5.jpg'
-    },
-    {
-      source: '/images/gallery_image6.jpg'
-    }
-  ];
+  const flickr = useRef(null);
   const ref = useRef(null);
   const articleRef = useRef(null);
+  const articleWidth = useRef(null);
+  const containerWidth = useRef(null);
   const [container, setContainer] = useState(0);
+
   const moveLeftArrow = () => {
-    if (container > -1600) {
-      ref.current.style.transform = `translateX(${container - articleRef.current}px)`;
-      setContainer((prev) => (prev += -400));
-    }
+    let num = container;
+    if (num + 410 > 0) return;
+    ref.current.style.transform = `translateX(${num + 410}px)`;
+    setContainer((prev) => (prev = prev + 410));
   };
   const moveRightArrow = () => {
-    if (container < 0) {
-      ref.current.style.transform = `translateX(${container + articleRef.current}px)`;
-      setContainer((prev) => (prev += 400));
-    }
+    let num = container;
+    if (num - 410 < -containerWidth.current * 4) return;
+    ref.current.style.transform = `translateX(${num - 410}px)`;
+    setContainer((prev) => (prev = prev - 410));
   };
+
+  const resizeWidth = () => {
+    containerWidth.current = parseInt(getComputedStyle(ref.current)['width']);
+
+    console.log(containerWidth.current);
+  };
+
+  flickr.current = useSelector((store) => store.flickrReducer.flickr);
+  if (flickr.current.length > 0) {
+    // articleWidth.current = parseInt(getComputedStyle(articleRef.current)['width']);
+  }
+
   useEffect(() => {
-    const width = parseInt(getComputedStyle(articleRef.current)['width']);
-    articleRef.current = width;
+    resizeWidth();
+    window.addEventListener('resize', resizeWidth);
+    //
+    return () => {
+      window.removeEventListener('resize', resizeWidth);
+    };
   }, []);
+  useEffect(() => {
+    if (articleWidth.current === null) return;
+    articleWidth.current = getComputedStyle(articleRef.current)['width'];
+  }, []);
+  
   return (
     <section className='gallery_main scrollContent'>
       <div className='inner'>
@@ -55,19 +61,20 @@ function GalleryMain(props) {
         <p>Go to Instagram</p>
         <div className='wrap'>
           <div className='container' ref={ref}>
-            {images.map((img, idx) => (
+            {flickr.current.map((img, idx) => (
               <article key={idx} ref={articleRef}>
-                <img src={process.env.PUBLIC_URL + img.source} alt='' />
+                <img
+                  src={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_m.jpg`}
+                  alt=''
+                />
               </article>
             ))}
           </div>
         </div>
-        {container > -1600 && (
-          <FontAwesomeIcon className='leftArrow' icon={faCaretLeft} onClick={moveLeftArrow} />
-        )}
-        {container < 0 && (
-          <FontAwesomeIcon className='rightArrow' icon={faCaretRight} onClick={moveRightArrow} />
-        )}
+
+        <FontAwesomeIcon className='leftArrow off' icon={faCaretLeft} onClick={moveLeftArrow} />
+
+        <FontAwesomeIcon className='rightArrow on' icon={faCaretRight} onClick={moveRightArrow} />
       </div>
     </section>
   );
