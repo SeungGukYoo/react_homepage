19aed3f443d660abe335338973065787c9a175a8
SeungGukYoo <dbtmd878@gmail.com>
2023-01-10 17:47:28 +0900

    feat: Info 문구 출력

---
+++ src/components/main/InfoPage/second_info.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function SecondInfo() {
+  return (
+    <>
+      <h1>
+        <span>Take a look&nbsp;</span>
+        <span>at our rooms</span>
+      </h1>
+      <p>
+        <span>The hotel is small with just three locations, but </span>
+        <span>more are in the making. Treat yourself to a </span>
+        <span>getaway in the Vipp Shelter surrounded by </span>
+        <span>Swedish nature, go for maximized urbanity at the </span>
+        <span>Vipp Loft in Copenhagen, or check into a hisoric </span>
+        <span>Copenhagen landmark at Vipp Chimney House.</span>
+      </p>
+    </>
+  );
+}
+
+export default SecondInfo;
