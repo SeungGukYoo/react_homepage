b0b08cceeabe37433b21985ec8ccdd7554864ee8
SeungGukYoo <dbtmd878@gmail.com>
2023-01-13 00:17:02 +0900

    fix: api Key, Id 값 env로 관리

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,23 +3,23 @@ import axios from 'axios';
 export const getYoutube = async (Info) => {
   let playId = '';
   if (Info.type === 'interest') {
-    playId = 'PL8Yp2hhTedFZOl_wbDTw7Pmx9iAxk6A48';
+    playId = `${process.env.REACT_APP_YOUTUBE_URL_INTEREST}`;
   }
   if (Info.type === 'attraction') {
-    playId = 'PL8Yp2hhTedFaB2O-x07LTTeScBKQkkKSb';
+    playId = `${process.env.REACT_APP_YOUTUBE_URL_ATTRACTION}`;
   }
   if (Info.type === 'food') {
-    playId = 'PL8Yp2hhTedFYamvuIbGJjdF_YI_nZ2vf-';
+    playId = `${process.env.REACT_APP_YOUTUBE_URL_FOOD}`;
   }
 
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playId}&maxResults=${Info.count}&key=AIzaSyCzFsvPUv4oQBwHeCsYH1FxMueWv-GAiY8`;
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playId}&maxResults=${Info.count}&key=${process.env.REACT_APP_YOUTUBE_KEY}`;
   return await axios.get(url);
 };
 
 export const getFlickr = async () => {
   const baseUrl = 'https://www.flickr.com/services/rest/?';
   const trand = 'flickr.interestingness.getList';
-  const key = '45c970bc3ee3b60b35d021e520358df0';
+  const key = process.env.REACT_APP_FLICKR_KEY;
   const url = `${baseUrl}method=${trand}&api_key=${key}&per_page=20&extras=date_taken&format=json&nojsoncallback=1`;
   return await axios.get(url);
 };
